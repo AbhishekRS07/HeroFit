9b572e222e655484c4cbe693a8b9a838989169d1
Abhishek <abhiraj812000@gmail.com>
2023-07-06 14:05:30 +0530

    TryNow Section and subscription pade added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,20 @@ import Footer from './Components/Footer/Footer';
 import Home from './Components/Homepage/Home';
 import Register from './Components/Login/Register';
 import Navbar from './Components/NavigationBar/Navbar';
+import Subscribe from './Components/Subscribe/Subscribe';
 import TrySection from './Components/TrySection/TrySection';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
-   <Home/>
-   <TrySection/>
-   <Footer/>
+    <Subscribe/>
+    <Footer/>
+   {/* <Home/>
+   <TrySection/> */}
+   
    {/* <Register/> */}
+   
     </div>
   );
 }
